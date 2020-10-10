d68640ec93024904435149d3bfdcdcf7616a3fea
leogirardeau <leogir.pro@gmail.com>
2020-10-10 12:25:34 +0200

    HomePage content

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { InfoSec } from './InfoSection.elements';
+import { Link } from 'react-router-dom';
+import { Container, Button } from '../../globalStyles';
+import { InfoSec, InfoRow, InfoColumn, TextWrapper, TopLine, Heading, Subtitle, ImgWrapper, Img } from './InfoSection.elements';
 
-const InfoSection = ({ lightBg }) => {
+const InfoSection = ({ primary, lightBg, imgStart, lightTopLine, lightTextDesc, buttonLabel, description, headLine, lightText, topLine, img, alt, start }) => {
     return (
         <>
             <InfoSec lightBg={lightBg}>
-                h1
+                <Container>
+                    <InfoRow imgStart={imgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
+                                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headLine}</Heading>
+                                <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                                <Link to='/sign-up'>
+                                    <Button big fontBig primary={primary}>
+                                        {buttonLabel}
+                                    </Button>
+                                </Link>
+                            </TextWrapper>
+                        </InfoColumn>
+                        <InfoColumn>
+                            <ImgWrapper start={start}>
+                                <Img src={img} alt={alt} />
+                            </ImgWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
             </InfoSec>
         </>
     );
--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,16 @@
 export const homeObjOne = {
-    lightBg: false
+    lightBg: false,
+    primary: true,
+    lightBg: false,
+    imgStart:'', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help yoi acheive your business goals', 
+    headLine: 'Lead Generation Specialist for Oneline Businesses', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    img: '', 
+    alt: 'Image', 
+    start: ''
 };
