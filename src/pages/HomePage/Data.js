456f60e48675337b742f9e8312ea01025221ad5e
leogirardeau <leogir.pro@gmail.com>
2020-10-11 09:56:33 +0200

    Homepage content

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,7 +1,6 @@
 export const homeObjOne = {
     lightBg: false,
     primary: true,
-    lightBg: false,
     imgStart:'', 
     lightTopLine: true, 
     lightTextDesc: true, 
@@ -13,4 +12,52 @@ export const homeObjOne = {
     img: require('../../images/svg-1.svg'), 
     alt: 'Image', 
     start: ''
+};
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: false,
+    imgStart:'start', 
+    lightTopLine: false, 
+    lightTextDesc: false, 
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help yoi acheive your business goals', 
+    headLine: 'Lead Generation Specialist for Oneline Businesses', 
+    lightText: false, 
+    topLine: 'Marketing Agency',
+    img: require('../../images/profil.JPG'), 
+    alt: 'Image', 
+    start: 'true'
+};
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart:'', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help yoi acheive your business goals', 
+    headLine: 'Lead Generation Specialist for Oneline Businesses', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    img: require('../../images/svg-2.svg'), 
+    alt: 'Image', 
+    start: ''
+};
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart:'start', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help yoi acheive your business goals', 
+    headLine: 'Lead Generation Specialist for Oneline Businesses', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    img: require('../../images/svg-3.svg'), 
+    alt: 'Image', 
+    start: 'true'
 };
