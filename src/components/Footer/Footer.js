13ec27620fb430c56e0735527c7dabc456842767
leogirardeau <leogir.pro@gmail.com>
2020-10-11 11:35:03 +0200

    Footer Content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import { 
+    FaFacebook, 
+    FaInstagram, 
+    FaYoutube, 
+    FaTwitter, 
+    FaLinkedin
+} from 'react-icons/fa';
 import { Button } from '../../globalStyles';
-import { FooterContainer, FooterSubscription, FooterSubHeading, FooterSubText, Form, FormInput } from './Footer.elements';
+import { 
+    FooterContainer, 
+    FooterSubscription, 
+    FooterSubHeading, 
+    FooterSubText, 
+    Form, 
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinksItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink,
+    SocialIcon
+} from './Footer.elements';
 
 const Footer = () => {
     return (
@@ -17,6 +43,67 @@ const Footer = () => {
                         <Button fontBig>Subscribe</Button>
                 </Form>
             </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                        <FooterLink to='/sign-up'>How it works</FooterLink>
+                        <FooterLink to='/'>Testimonials</FooterLink>
+                        <FooterLink to='/'>Carrers</FooterLink>
+                        <FooterLink to='/'>Investors</FooterLink>
+                        <FooterLink to='/'>Terms of Service</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                        <FooterLink to='/'>Contact</FooterLink>
+                        <FooterLink to='/'>Support</FooterLink>
+                        <FooterLink to='/'>Destination</FooterLink>
+                        <FooterLink to='/'>Sponsorships</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                        <FooterLink to='/'>Submit Video</FooterLink>
+                        <FooterLink to='/'>Ambassadors</FooterLink>
+                        <FooterLink to='/'>Agency</FooterLink>
+                        <FooterLink to='/'>Influencer</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>Social Medial</FooterLinkTitle>
+                        <FooterLink to='/'>Instagram</FooterLink>
+                        <FooterLink to='/'>Facebook</FooterLink>
+                        <FooterLink to='/'>Youtube</FooterLink>
+                        <FooterLink to='/'>Twitter</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to="/">
+                        <SocialIcon/>
+                        ULTRA
+                    </SocialLogo>
+                    <WebsiteRights>ULTRA 2020</WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href='/' target="_blank" arial-label="Facebook">
+                            <FaFacebook />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target="_blank" arial-label="Instagram">
+                            <FaInstagram />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target="_blank" arial-label="Youtube">
+                            <FaYoutube />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target="_blank" arial-label="Twitter">
+                            <FaTwitter />
+                        </SocialIconLink>
+                        <SocialIconLink href='/' target="_blank" arial-label="Linkedin">
+                            <FaLinkedin />
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
         </FooterContainer>
     )
 }
